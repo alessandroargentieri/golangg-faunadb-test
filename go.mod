503daef8e21290317cade842f3a1f0722b2ba0a4
alessandroargentieri <alexmawashi87@gmail.com>
2020-12-23 07:58:20 +0100

    first

---
+++ go.mod
@@ -0,0 +1,9 @@
+module faunago
+
+go 1.15
+
+require (
+	github.com/fauna/faunadb-go/v3 v3.0.0 // indirect
+	github.com/faunadb/faunadb-go v2.0.0+incompatible // indirect
+	github.com/kubeless/kubeless v1.0.7 // indirect
+)
---
+++ main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+    "log"
+    "os"
+    "encoding/json"
+    //"github.com/kubeless/kubeless/pkg/functions"
+    f "github.com/fauna/faunadb-go/v3/faunadb"
+)
+
+
+type Pet struct {
+     Name string `fauna:"name", json:"name, omitempty`
+     Age  int    `fauna:"age" , json:"age,  omitempty`
+}
+
+func main() {
+    log.Println("ok")
+    secret := os.Getenv("SECRET")
+    dbUrl  := os.Getenv("FAUNA_URL") //http://localhost:8443
+
+    client := f.NewFaunaClient(secret, f.Endpoint(dbUrl))
+    
+   // log.Println(event.Data)
+   inputPet := `{"name":"Fuffy", "age":4}`
+
+    var pet Pet   
+    json.Unmarshal([]byte(inputPet), &pet)
+
+    client.Query(
+        f.Create(
+            f.Class("pets"),
+            f.Obj{"data": pet},
+        ),
+    )
+
+    res, _ := client.Query(
+           f.Get(
+            f.MatchTerm(
+                f.Index("pet_by_name"),
+                "Fuffy",
+            ),
+        ),
+     )
+
+     var pet2 Pet
+
+     res.At(f.ObjKey("data")).Get(&pet2)
+     log.Println(pet2)
+
+     data, _ := json.Marshal(pet2)
+
+    log.Println(string(data))
+
+}
